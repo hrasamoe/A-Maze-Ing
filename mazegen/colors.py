998807f4bc94b2046589b705183736d10e94a419
Heritiana <herysamuelpljv@gmail.com>
2026-04-30 17:07:20 +0300

    remove green color

--- mazegen/colors.py
+++ mazegen/colors.py
@@ -1,7 +1,6 @@
 RESET: str = "\033[0m"
 BLACK: str = "\033[30m"
 RED: str = "\033[31m"
-GREEN: str = "\033[32m"
 YELLOW: str = "\033[33m"
 BLUE: str = "\033[34m"
 MAGENTA: str = "\033[35m"
@@ -9,4 +8,4 @@ CYAN: str = "\033[36m"
 WHITE: str = "\033[37m"
 
 
-COLOR_PALETTE: list[str] = [WHITE, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN]
+COLOR_PALETTE: list[str] = [WHITE, RED, YELLOW, BLUE, MAGENTA, CYAN]
--- mazegen/config.py
+++ mazegen/config.py
@@ -120,11 +120,11 @@ class MazeConfig:
     def logic_chek(self,) -> None:
         if self.property.entry == self.property.exit:
             raise ValueError("Entry and Exit coordinates cannot be identical")
-        x1, y2 = self.property.entry
+        x1, y1 = self.property.entry
         x2, y2 = self.property.exit
-        if (0 <= x1 < self.property.width
-                and 0 <= y2 < self.property.height):
+        if not (0 <= x1 < self.property.width
+                and 0 <= y1 < self.property.height):
             raise ValueError(f"Entry {self.property.entry} is out of bounds")
-        if (0 <= x2 < self.property.width
+        if not (0 <= x2 < self.property.width
                 and 0 <= y2 < self.property.height):
             raise ValueError(f"Exit {self.property.exit} is out of bounds.")
