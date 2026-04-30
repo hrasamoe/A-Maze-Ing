14f7461a1ad1864dfa8274f628cd723dce70f5a0
Heritiana <herysamuelpljv@gmail.com>
2026-04-30 11:51:32 +0300

    list of colors

---
+++ mazegen/colors.py
@@ -0,0 +1,12 @@
+RESET: str = "\033[0m"
+BLACK: str = "\033[30m"
+RED: str = "\033[31m"
+GREEN: str = "\033[32m"
+YELLOW: str = "\033[33m"
+BLUE: str = "\033[34m"
+MAGENTA: str = "\033[35m"
+CYAN: str = "\033[36m"
+WHITE: str = "\033[37m"
+
+
+COLOR_PALETTE: list[str] = [WHITE, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN]
