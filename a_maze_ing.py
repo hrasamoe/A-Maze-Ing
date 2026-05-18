6c0ca103a655026b960fbc445c1c81cf318e16f3
Heritiana <herysamuelpljv@gmail.com>
2026-05-18 11:29:47 +0300

    Adding try catch on render

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,4 +1,7 @@
 from mazegen.mlx_window import Renderer
 
 if __name__ == "__main__":
-    Renderer().run()
+    try:
+        Renderer().run()
+    except Exception as e:
+        print(f"An error was occured {e}")
