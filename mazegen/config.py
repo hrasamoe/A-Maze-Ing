65d004c095677183fbebc60e8d026ce5c1c8200c
Ny Aina Tsilavina Razafimahatratra <ny-araza@c1r8s4.42antananarivo.mg>
2026-07-27 10:35:43 +0300

    add Field to width and heigh in configModel

--- mazegen/config.py
+++ mazegen/config.py
@@ -1,17 +1,19 @@
+import os
+import stat
 from operator import le
 from typing import Any
 from unittest.loader import defaultTestLoader
+
 from pydantic import BaseModel, Field, ValidationError
-import os
-import stat
 
 
 class MazeProperty(BaseModel):
     """
-        Model used for data validation in config.txt
+    Model used for data validation in config.txt
     """
-    width: int = Field(...)
-    height: int = Field(...)
+
+    width: int = Field(..., ge=18, le=36, description="Maze with between 18 and 36")
+    height: int = Field(..., ge=9, le=18, description="Maze height between 9 and 18")
     entry: tuple[int, int]
     exit: tuple[int, int]
     output_file: str = Field(default="maze_output.txt", min_length=2)
@@ -23,13 +25,14 @@ class MazeProperty(BaseModel):
 
 class MazeConfig:
     """
-        All operations related to data
-        validation are described in config.txt:
+    All operations related to data
+    validation are described in config.txt:
 
-        methods:
-            - logic_check
-            - read_file
+    methods:
+        - logic_check
+        - read_file
     """
+
     def __init__(self, filepath: str) -> None:
         self.filepath: str = filepath
         raw_data = self.read_file(filepath)
@@ -39,7 +42,7 @@ class MazeConfig:
         except ValidationError as e:
             occured_error = []
             for err in e.errors():
-                field_name = str(err.get('loc')).upper()
+                field_name = str(err.get("loc")).upper()
                 error_type = err.get("type")
                 if error_type == "missing":
                     occured_error.append(
@@ -48,20 +51,18 @@ class MazeConfig:
                     )
                 elif error_type in ["type_error.integer", "int_parsing"]:
                     occured_error.append(
-                        f"[!] Invalid value: {field_name} must be "
-                        "a number"
+                        f"[!] Invalid value: {field_name} must be a number"
                     )
                 elif error_type in ["greater_than_equal", "less_than_equal"]:
                     if field_name == "WIDTH":
                         occured_error.append(
                             f"[!] Out of Bounds: '{field_name}' must be "
                             "between 16 and 35."
-                            )
+                        )
                     if field_name == "HEIGHT":
                         occured_error.append(
-                            f"[!] Out of bounds: '{field_name}' must be"
-                            "between 8 and 16"
-                            )
+                            f"[!] Out of bounds: '{field_name}' must bebetween 8 and 16"
+                        )
                 elif error_type == "string_too_short":
                     occured_error.append(
                         f"[!] Invalid value: '{field_name}' cannot be empty"
@@ -82,9 +83,7 @@ class MazeConfig:
                         f"[!] [ERROR] '{field_name}': {err.get('msg')}"
                     )
             error_str = "\n".join(occured_error)
-            raise ValueError(
-                f"Validation failed in '{filepath}' : \n{error_str}"
-            )
+            raise ValueError(f"Validation failed in '{filepath}' : \n{error_str}")
         self.logic_chek()
         self.width = self.property.width
         self.height = self.property.height
@@ -104,8 +103,8 @@ class MazeConfig:
 
     def read_file(self, filepath: str) -> dict[str, Any]:
         """
-            Read the config.txt file,
-            storing all its data in a dictionary.
+        Read the config.txt file,
+        storing all its data in a dictionary.
         """
         data: dict[str, Any] = {}
         try:
@@ -114,14 +113,14 @@ class MazeConfig:
                 all_read_rigth = stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
                 if (actual_status & all_read_rigth) != all_read_rigth:
                     os.chmod(filepath, actual_status | all_read_rigth)
-            with open(filepath, 'r') as fd:
+            with open(filepath, "r") as fd:
                 for line in fd:
                     line = line.strip()
-                    if not line or line.startswith('#'):
+                    if not line or line.startswith("#"):
                         continue
-                    if '=' not in line:
+                    if "=" not in line:
                         raise Exception(f"Invalid line format: {line}")
-                    key, value = line.split('=', 1)
+                    key, value = line.split("=", 1)
                     key = key.strip().upper()
                     value = value.strip()
 
@@ -129,11 +128,10 @@ class MazeConfig:
                         data[key.lower()] = ""
                         continue
                     if key in ["ENTRY", "EXIT"]:
-                        coords = value.split(',')
+                        coords = value.split(",")
                         if len(coords) == 2:
                             try:
-                                data[key.lower()] = (
-                                    int(coords[0]), int(coords[1]))
+                                data[key.lower()] = (int(coords[0]), int(coords[1]))
                             except Exception as e:
                                 print(f"Unexcepted error occured {e}")
                         else:
@@ -147,18 +145,18 @@ class MazeConfig:
         except OSError as e:
             raise OSError(f"[ERROR PARSING FILE CONFIG]: {e}")
 
-    def logic_chek(self,) -> None:
+    def logic_chek(
+        self,
+    ) -> None:
         """
-            Check if the entry point and exit
-            point are within the maze and do not overlap
+        Check if the entry point and exit
+        point are within the maze and do not overlap
         """
         if self.property.entry == self.property.exit:
             raise ValueError("Entry and Exit coordinates cannot be identical")
         x1, y1 = self.property.entry
         x2, y2 = self.property.exit
-        if not (0 <= x1 < self.property.width
-                and 0 <= y1 < self.property.height):
+        if not (0 <= x1 < self.property.width and 0 <= y1 < self.property.height):
             raise ValueError(f"Entry {self.property.entry} is out of bounds")
-        if not (0 <= x2 < self.property.width
-                and 0 <= y2 < self.property.height):
+        if not (0 <= x2 < self.property.width and 0 <= y2 < self.property.height):
             raise ValueError(f"Exit {self.property.exit} is out of bounds.")
